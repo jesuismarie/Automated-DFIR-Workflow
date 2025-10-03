5bb38231e077f5499203db51370e8d4139cd0cba
Marie <mari.nazaryan7173@gmail.com>
2025-10-03 19:46:22 +0400

    add constants

---
+++ constants.py
@@ -0,0 +1,19 @@
+ARCHIVE_EXTENSIONS = [
+	'.zip',
+	'.rar',
+	'.7z',
+	'.tar',
+	'.gz',
+	'.bz2',
+	'.tgz'
+]
+
+TEMP_EXTENSIONS = [
+	'.crdownload',
+	'.part',
+	'.download',
+	'.opdownload',
+	'.!ut', '.bc!', '.xltd',
+	'.filepart',
+	'.tmp', '.unfinished', '.aria2'
+]
