ef74f1a6d5c5c77c6c5dc48ddd5e2defcfdb14ed
Marie <mari.nazaryan7173@gmail.com>
2025-10-09 22:48:23 +0400

    add constants

--- constants.py
+++ constants.py
@@ -12,8 +12,17 @@ TEMP_EXTENSIONS = [
 	'.crdownload',
 	'.part',
 	'.download',
+	'.inprogress',
+	'._mp', '.partial',
+	'.dms', '.bak',
 	'.opdownload',
 	'.!ut', '.bc!', '.xltd',
 	'.filepart',
 	'.tmp', '.unfinished', '.aria2'
 ]
+
+# Directory where archives will be safely extracted for analysis
+UNPACK_ROOT_DIR = 'unpacked_analysis_queue'
+
+# Maximum number of files to extract from a single archive (to prevent zip bombs)
+MAX_FILES_TO_EXTRACT = 100
